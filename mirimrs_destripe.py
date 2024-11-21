87aeaf5cb854948007e5ca9662bc115d1fe3811a
Elisabeth Matthews <matthews@mpia.de>
2024-11-21 12:20:09 +0100

    v0 -> this is redone into a quick OOP, but not yet tested. good start/restart point if the finetuning goes wrong.

--- mirimrs_destripe.py
+++ mirimrs_destripe.py
@@ -25,41 +25,30 @@ if not os.path.isdir("plots"):
     os.mkdir("plots")
 
 
-## TODO check with HK, PP what exact radius is being used
+## OOP !!!
 
-for i_file, file in enumerate(filelist):
-    print(file)
-    print(file.replace("s3d","x1d"))
-    print("")
+class CubeDestripe():
+    def __init__(self, file_ifualign):
 
-    cube = fits.open(file)
-    spec = fits.open(file.replace("s3d","x1d"))
-    spec_2r = fits.open(file.replace("s3d","x1d").replace("211123","170124_ifur2"))
-    cube_ifualign = fits.open(file.replace("211123","2301024_ifualign"))
-    cube_ifualign_copy = fits.open(file.replace("211123","2301024_ifualign"))
+        self.cube_ifualign = fits.open(file_ifualign)
+        self.cube_copy = fits.open(file_ifualign)
 
-    out_cube_name = "flat_cubefiles/" + file.replace("s3d","s3d_elisabethfix").split("/")[-1]
+        ## TODO better out name
+        out_cube_name = "flat_cubefiles/" + file_ifualign.replace("s3d","s3d_elisabethfix").split("/")[-1]
 
-    if not cube["SCI"].header["BUNIT"] == "MJy/sr":
-        raise ValueError(f"{file} is not in MJy/sr")
-
-    if file == filelist[0]:
-        cube.info()
-        spec.info()
-        spec_2r.info()
-        print(spec["EXTRACT1D"].columns)
-        print(spec_2r["EXTRACT1D"].columns)
+        ## diagnostics
+        if not self.cube_ifualign["SCI"].header["BUNIT"] == "MJy/sr":
+            raise ValueError(f"{file_ifualign} is not in MJy/sr")
 
 
-    for masking in [2]:
+    ## function 1: mask pixels!
+    def mask_source(self):
 
         ## DO SOME MASKING on IFUALIGN
-        cube_sci = cube_ifualign["SCI"].data
+        cube_sci = self.cube_ifualign["SCI"].data
         cube_sci_mask = cube_sci.copy()
-        cube_sci_nomask = cube_sci.copy()
-        cube_sci_slicewise = cube_sci.copy()
 
-        mask_rad = 4
+        mask_rad = 4 ## TODO throughout need to find hardcoding stuff !!! 
         ##
         im_sci = np.sum(cube_sci, axis=0)
 
@@ -75,56 +64,47 @@ for i_file, file in enumerate(filelist):
             im_sci = np.sum(cube_sci_mask, axis=0)
             # im_sci[18,24] = 100_000
 
-        cube_sci_mask_slicewise = cube_sci_mask.copy()
+        self.cube_sci = cube_sci
+        self.cube_sci_mask = cube_sci_mask
+
+        ## TODO need a diagnostic plot of cube_sci_mask i thinkkk
+
+    ## function 2: rows, columns, flat
+    def destripe(self):
 
         ## here we destripe
 
-        ## first do columns
-        ymean = np.nanmean(im_sci, axis=1)
-        sub1 = np.array(ymean/cube_sci.shape[0])[None,:,None]
-        sub1 = np.repeat(sub1,cube_sci.shape[0],axis=0)
-        sub1 = np.repeat(sub1,cube_sci.shape[2],axis=2) ## TODO can those three lines be slicker code?
-
-        cube_sci_mask = cube_sci_mask - sub1
-        cube_sci_nomask = cube_sci_nomask - sub1
-        # plt.imshow(np.sum(sub, axis=0), origin="lower")
-        im_sci = np.sum(cube_sci_mask, axis=0)
-
-        ## then rows
-        xmean = np.nanmean(im_sci, axis=0)
-        sub2 = np.array(xmean/cube_sci.shape[0])[None,None,:]
-        sub2 = np.repeat(sub2,cube_sci.shape[0],axis=0)
-        sub2 = np.repeat(sub2,cube_sci.shape[1],axis=1) ## TODO can those three lines be slicker code?
-
-        cube_sci_mask = cube_sci_mask - sub2
-        cube_sci_nomask = cube_sci_nomask - sub2
-        im_sci = np.sum(cube_sci_mask, axis=0)
-        # im_sci[18,24] = 100_000
-
         ## slicebyslice_mask
-        for q in range(cube_sci.shape[0]):
+        for q in range(self.cube_sci_mask.shape[0]):
 
-            ymean = np.nanmean(cube_sci_mask_slicewise[q,:,:], axis=1) ## BLA BLA ax stuff #BLA this is corr (??)
+            ymean = np.nanmean(self.cube_sci_mask[q,:,:], axis=1) ## BLA BLA ax stuff #BLA this is corr (??)
             sub1 = np.array(ymean)[:,None]
-            sub1 = np.repeat(sub1,cube_sci.shape[2],axis=1)
-            cube_sci_mask_slicewise[q,:,:] = cube_sci_mask_slicewise[q,:,:] - sub1
-            cube_sci_slicewise[q,:,:] = cube_sci_slicewise[q,:,:] - sub1
+            sub1 = np.repeat(sub1,self.cube_sci_mask.shape[2],axis=1)
+            self.cube_sci_mask[q,:,:] = self.cube_sci_mask[q,:,:] - sub1
+            self.cube_sci[q,:,:] = self.cube_sci[q,:,:] - sub1
             # print(np.nanmedian(cube_sci_mask_slicewise[q,:,:]))
             #
-            xmean = np.nanmean(cube_sci_mask_slicewise[q,:,:], axis=0)
+            xmean = np.nanmean(self.cube_sci_mask[q,:,:], axis=0)
             sub2 = np.array(xmean)[None,:]
-            sub2 = np.repeat(sub2,cube_sci.shape[1],axis=0)
-            cube_sci_mask_slicewise[q,:,:] = cube_sci_mask_slicewise[q,:,:] - sub2
-            cube_sci_slicewise[q,:,:] = cube_sci_slicewise[q,:,:] - sub2
+            sub2 = np.repeat(sub2,self.cube_sci_mask.shape[1],axis=0)
+            self.cube_sci_mask[q,:,:] = self.cube_sci_mask[q,:,:] - sub2
+            self.cube_sci[q,:,:] = self.cube_sci[q,:,:] - sub2
             # print(np.nanmedian(cube_sci_mask_slicewise[q,:,:]))
             # print("~")
 
-        im_sci = np.sum(cube_sci_mask_slicewise, axis=0) ## TODO this is still a mess !! IS NOT PLOTTING THE IMAGE CORRECTLY <<<<<
+        im_sci = np.sum(self.cube_sci, axis=0) ## TODO this is still a mess !! IS NOT PLOTTING THE IMAGE CORRECTLY <<<<<
+        im_sci_mask = np.sum(self.cube_sci_mask, axis=0)
 
         ## save cube_sci_slicewise!!
-        cube_ifualign_copy["SCI"].data = cube_sci_slicewise         
-        cube_ifualign_copy.writeto(out_cube_name,overwrite=True)
+        self.cube_copy["SCI"].data = self.cube_sci        
+        self.cube_copy.writeto(self.out_cube_name,overwrite=True)
 
+
+
+    ## function 3: make the plots
+    def diagnostic_plots(self):
+
+        return None ## TODO TODOOOOOO diagnostic plots !!! (split per function ??)
 
         # Create a figure with a 2x3 grid using gridspec
         fig = plt.figure(figsize=(7, 7))
@@ -209,29 +189,18 @@ for i_file, file in enumerate(filelist):
 
         plt.savefig(f"plots/backgroundvariance_{file[file.index('ch'):file.index('s3d')-1]}_mask{masking}.pdf")
 
-    # plt.show()
-    plt.close("all")
+        # plt.show()
+        plt.close("all")
 
-    if i_file >= 2:
+
+
+filelist = sorted(glob.glob("../W0458/data_reprocessed211123/*s3d*"))
+print(filelist)
+
+for i_file, file in enumerate(filelist):
+    cube_destripe = CubeDestripe(file)
+    cube_destripe.mask_sources()
+    cube_destripe.destripe() ## I have to pass the correct cube here somehow there's some interdependency ... but check this even works, first. TODO !
+
+    if i_file > 3:
         break
-    break
-
-
-
-## TODO add uncert
-
-# TODO HK PP is the script you use to go from s3d to x1d shareable? I want to think more about this <<<
-    
-# TODO HK cuts of spectra at 0 ... that's weird (!) what do we make of that?
-    
-# TODO save figures !!
-    
-# TODO a bunch with different radii !!
-    
-## The radius is quite instrumental to the spectrum ... that *must* indicate that something is going wrong with the background correction.'
-
-
-
-
-## TODO NEXT you have to save the spectrum with some (["ERR"]) uncerts for a ~fair comparison !
-## we're getting there, need to plot the uncerts in all the specy-plots. YOUAREHERE
